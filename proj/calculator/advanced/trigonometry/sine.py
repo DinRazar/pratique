f1abcc1b738f0c120c71861b8124d7860502ba0c
Dmitriy <108523468+DinRazar@users.noreply.github.com>
2025-10-28 10:23:26 +0300

    Что-то обновил
    Хочу спать

--- proj/calculator/advanced/trigonometry/sine.py
+++ proj/calculator/advanced/trigonometry/sine.py
@@ -1,4 +1,13 @@
 import math
-
 def sin(x):
     return math.sin(x)
+
+# from ..root import square_root  
+# def sin(x):
+#     print(f"Квадратный корень из 49: {square_root(49)}")
+#     return math.sin(x)
+
+# from calculator.advanced.root import square_root 
+# def sin(x):
+#     print(f"Квадратный корень из 16: {square_root(16)}") 
+#     return math.sin(x)
